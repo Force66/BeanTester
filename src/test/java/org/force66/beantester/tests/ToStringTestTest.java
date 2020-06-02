40d46f23c281a7c034fb4a3487826a8edd925b1f
mattgarner <matt.garner@boothen.co.uk>
2020-06-02 17:01:48 +0100

    Added empty String check in ToStringTest as that is a new Pitest check.

--- src/test/java/org/force66/beantester/tests/ToStringTestTest.java
+++ src/test/java/org/force66/beantester/tests/ToStringTestTest.java
@@ -2,9 +2,9 @@
  * This software is licensed under the Apache License, Version 2.0
  * (the "License") agreement; you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ToStringTestTest {
-	
+
 	ToStringTest test;
 	ValueGeneratorFactory factory;
 
@@ -32,7 +32,7 @@ public class ToStringTestTest {
 	@Test
 	public void test() {
 		Assert.assertTrue(test.testBeanClass(TestBean.class, null) == true);
-		
+
 		TestBean.exceptionToThrow = new RuntimeException("splat");
 		try {
 			test.testBeanClass(TestBean.class, null);
@@ -41,29 +41,35 @@ public class ToStringTestTest {
 		catch (Exception e) {
 			Assert.assertTrue(e.getMessage().contains("toString() failed execution"));
 		}
-		
+
 		TestBean.exceptionToThrow = null;
 		TestBean.calledWithValue = false;
 		test.testBeanClass(TestBean.class, null);
 		Assert.assertTrue(TestBean.calledWithValue);
-		
+
 		TestBean.exceptionToThrow = null;
 		TestBean.fieldExceptionToThrow = new RuntimeException("splat");
 		test.setFailureReason("foo");
 		Assert.assertTrue(test.testBeanClass(TestBean.class, null) == true);
 		Assert.assertTrue(test.getFailureReason() == null);
-		
-		TestBean.toStringNullInd = true;		
+
+		TestBean.toStringNullInd = true;
 		Assert.assertTrue(test.testBeanClass(TestBean.class, null) == false);
 		Assert.assertTrue(test.getFailureReason() != null);
-		
+
+                TestBean.toStringNullInd = false;
+                TestBean.toStringEmptyInd = true;
+                Assert.assertTrue(test.testBeanClass(TestBean.class, null) == false);
+                Assert.assertTrue(test.getFailureReason() != null);
+
 	}
-	
+
 	public static class TestBean {
-		
+
 		private static RuntimeException exceptionToThrow=null;
 		private static RuntimeException fieldExceptionToThrow=null;
 		private static boolean toStringNullInd = false;
+		private static boolean toStringEmptyInd = false;
 		private static boolean calledWithValue = false;
 		private String bogusField;
 
@@ -72,7 +78,14 @@ public class ToStringTestTest {
 			if (exceptionToThrow != null) {
 				throw exceptionToThrow;
 			}
-			if (toStringNullInd)   return null;
+
+			if (toStringNullInd) {
+				return null;
+			}
+
+			if (toStringEmptyInd) {
+			        return "";
+                        }
 			return super.toString();
 		}
 
@@ -89,7 +102,7 @@ public class ToStringTestTest {
 			}
 			this.bogusField = bogusField;
 		}
-		
+
 	}
 
 }
